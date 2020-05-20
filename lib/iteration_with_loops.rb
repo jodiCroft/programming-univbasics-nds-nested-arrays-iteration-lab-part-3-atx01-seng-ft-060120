457c60a35a958ede8d484f08467c1a47bdc7da3a
Jodi Croft <jodipruett@gmail.com>
2020-05-20 18:31:37 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,11 +1,11 @@
 def join_nested_strings(aoa_strings_and_nums)
-  all_the_strings = ""
+  all_the_strings = " "
   row_index = 0
   while row_index < aoa_strings_and_nums.length do
     element_index = 0
     while element_index < aoa_strings_and_nums[row_index].length do
       if aoa_strings_and_nums[row_index][element_index].class == String
-        all_the_strings.push(aoa_strings_and_nums[row_index][element_index])
+        all_the_strings << aoa_strings_and_nums[row_index][element_index]
         element_index += 1
     end
     row_index += 1
